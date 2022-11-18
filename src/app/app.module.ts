d894b86fa7815021ff2c5741fc9d31132c3710f5
Facundo Godoy <facundo5_94@hotmail.com>
2022-11-18 11:25:13 -0300

    Primer Desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
+import { ToolbarComponent } from './views/toolbar/toolbar.component';
+import { MainComponent } from './views/main/main.component';
+import { FooterComponent } from './views/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
